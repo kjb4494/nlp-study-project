e0b1fe29c22b4f6dbca6b821f0038da771dbd3ca
kjb4494 <rlagnltn1357@gmail.com>
2020-07-14 16:15:54 +0900

    훈련 모듈 끝

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -61,6 +61,7 @@ class CVAETrainer:
                 exp_epoch_start_point = 0
 
         for epoch in range(exp_epoch_start_point, self.epoch):
+            start_time = time.time()
             train_output = self._run_one_epoch(
                 step_function=self.train_step,
                 data_loader=train_data_loader,
@@ -68,9 +69,18 @@ class CVAETrainer:
                 epoch=epoch,
                 is_train=True
             )
+            elapsed_time = time.time() - start_time
+            self.report_per_epoch(
+                metrics=train_output,
+                mode_name='Train',
+                epoch=epoch,
+                elapsed_time=elapsed_time,
+                is_train=True
+            )
             output_report = {"train": train_output}
 
             if valid_data_loader:
+                start_time = time.time()
                 valid_output = self._run_one_epoch(
                     step_function=self.valid_step,
                     data_loader=valid_data_loader,
@@ -78,8 +88,18 @@ class CVAETrainer:
                     epoch=epoch,
                     is_train=False
                 )
+                elapsed_time = time.time() - start_time
+                self.report_per_epoch(
+                    metrics=valid_output,
+                    mode_name='Valid',
+                    epoch=epoch,
+                    elapsed_time=elapsed_time,
+                    is_train=False
+                )
+                output_report['valid'] = valid_output
 
             if test_data_loader:
+                start_time = time.time()
                 test_output = self._run_one_epoch(
                     step_function=self.test_step,
                     data_loader=test_data_loader,
@@ -87,6 +107,19 @@ class CVAETrainer:
                     epoch=epoch,
                     is_train=False
                 )
+                elapsed_time = time.time() - start_time
+                self.report_per_epoch(
+                    metrics=test_output,
+                    mode_name='Test',
+                    epoch=epoch,
+                    elapsed_time=elapsed_time,
+                    is_train=False
+                )
+                output_report['test'] = test_output
+
+            output_reports.append(output_report)
+            if epoch % self.save_epoch_step == 0:
+                torch.save(self.model.state_dict(), self.model_path.format(epoch))
 
         return output_reports
 
@@ -98,8 +131,6 @@ class CVAETrainer:
         for step_id, model_input in enumerate(iterator):
             current_step_num = current_step_num + 1
             step_output = step_function(model_input, current_step_num)
-            # 각 스탭의 리포트 구현은 나중에...
-            # self.report_per_step(step_output, step_id, mode, epoch, is_train=is_train)
             step_outputs.append(step_output)
 
         if is_train and self.is_learning_decay:
@@ -180,3 +211,66 @@ class CVAETrainer:
     def update_gradient(self, loss):
         loss.backward()
         self.optimizer.step()
+
+    def report_per_epoch(self, metrics, mode_name, epoch=None, elapsed_time=0.0, is_train=True):
+        def print_with_logging(file_writer, log_message):
+            file_writer.write(log_message + '\n')
+            print(log_message)
+        log_file_name = self.log_path.format(epoch)
+        log_writer = open(log_file_name, 'w')
+
+        # 확인 필요한 코드
+        losses = [metric['loss'] for metric in metrics]
+        loss = {}
+        for key in losses[0].keys():
+            loss[key] = 0.0
+            for l in losses:
+                if type(l[key]) != float:
+                    loss[key] += l[key].item()
+                else:
+                    loss[key] += l[key]
+            loss[key] /= len(losses)
+        metric = []
+        for key, value in loss.items():
+            metric.append('{0}: {1:.3f}'. format(key, value))
+        metric_str = ', '.join(metric)
+
+        elapsed_time_str = mode_name + ' elapsed Time for Epoch {0} %H:%M:%S'.format(epoch)
+        elapsed_time_str = str(time.strftime(elapsed_time_str, time.gmtime(elapsed_time)))
+        print_with_logging(log_writer, elapsed_time_str)
+
+        metric_title = 'Metric in {0} set for Epoch #{1}: {2}'.format(mode_name, epoch, metric_str)
+        print_with_logging(log_writer, metric_title)
+
+        epoch_ex_str = "========== {0} Examples for Epoch #{1} ==========".format(mode_name, epoch)
+        print_with_logging(log_writer, epoch_ex_str)
+
+        for i in range(self.num_samples):
+            context_sents = metrics[0]["model_output"]["context_sents"][i]
+            for turn_id, turn in enumerate(context_sents):
+                context_turn_str = "Context Turn #{0}: {1}".format(turn_id, turn)
+                print_with_logging(log_writer, context_turn_str)
+
+            generated_sent = metrics[0]["model_output"]["output_sents"][i]
+            generated_sent_str = "Generated (Sample #1): {0}".format(generated_sent)
+            print_with_logging(log_writer, generated_sent_str)
+
+            if not is_train:
+                for j in range(self.num_samples - 1):
+                    sampled_sent = metrics[0]["model_output"]["sampled_output_sents"][j][i]
+                    sampled_sent_str = "Sample #{0}: {1}".format(j + 2, sampled_sent)
+                    print_with_logging(log_writer, sampled_sent_str)
+                if self.is_test_multi_da:
+                    for da in self.da_types:
+                        multi_da_result = metrics[0]["model_output"]["ctrl_output_sents"][da][i]
+                        multi_da_str = "{0} : {1}".format(da, multi_da_result)
+                        print_with_logging(log_writer, multi_da_str)
+
+            real_str = metrics[0]["model_output"]["real_output_sents"][i]
+            predicted_da = metrics[0]["model_output"]["output_das"][i]
+            real_da = metrics[0]["model_output"]["real_output_das"][i]
+
+            print_with_logging(log_writer, "Real Response: {0}".format(real_str))
+            print_with_logging(log_writer, "Predicted DA: {0}".format(predicted_da))
+            print_with_logging(log_writer, "Real DA: {0}".format(real_da))
+        log_writer.close()
